8b73cd4ad55bc06954072f1275f308e1d0e0cd64
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2025-01-15 10:12:17 +0800

    item

--- src/main/java/com/besson/tutorial/TutorialMod.java
+++ src/main/java/com/besson/tutorial/TutorialMod.java
@@ -1,5 +1,6 @@
 package com.besson.tutorial;
 
+import com.besson.tutorial.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -19,6 +20,7 @@ public class TutorialMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+		ModItems.registerItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
