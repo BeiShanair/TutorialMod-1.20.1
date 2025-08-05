5782faef4be4ef5b4ee70a31e2af2b9b42b7b1ae
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2025-08-05 15:17:36 +0800

    flower gen

--- src/main/java/com/besson/tutorial/world/ModConfiguredFeatures.java
+++ src/main/java/com/besson/tutorial/world/ModConfiguredFeatures.java
@@ -7,10 +7,7 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.ConstantIntProvider;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.ConfiguredFeatures;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.TreeFeatureConfig;
+import net.minecraft.world.gen.feature.*;
 import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
 import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
@@ -18,6 +15,8 @@ import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
 
 public class ModConfiguredFeatures {
     public static final RegistryKey<ConfiguredFeature<?, ?>> ICE_ETHER_TREE_KEY = of("ice_ether_tree");
+
+    public static final RegistryKey<ConfiguredFeature<?, ?>> SIMPLE_FLOWER_KEY = of("simple_flower");
 
     public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> featureRegisterable) {
         ConfiguredFeatures.register(featureRegisterable, ICE_ETHER_TREE_KEY, Feature.TREE,
@@ -28,6 +27,11 @@ public class ModConfiguredFeatures {
                         new BlobFoliagePlacer(ConstantIntProvider.create(3), ConstantIntProvider.create(2), 3),
                         new TwoLayersFeatureSize(1, 0, 2)
                 ).build());
+
+        ConfiguredFeatures.register(featureRegisterable, SIMPLE_FLOWER_KEY, Feature.FLOWER,
+                new RandomPatchFeatureConfig(20, 4, 3,
+                        PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
+                                new SimpleBlockFeatureConfig(BlockStateProvider.of(ModBlocks.SIMPLE_FLOWER)))));
     }
 
     public static RegistryKey<ConfiguredFeature<?, ?>> of(String id) {
--- src/main/java/com/besson/tutorial/world/ModWorldGeneration.java
+++ src/main/java/com/besson/tutorial/world/ModWorldGeneration.java
@@ -1,9 +1,11 @@
 package com.besson.tutorial.world;
 
+import com.besson.tutorial.world.flower.ModFlowerGeneration;
 import com.besson.tutorial.world.tree.ModTreeGeneration;
 
 public class ModWorldGeneration {
     public static void register(){
         ModTreeGeneration.registerTrees();
+        ModFlowerGeneration.registerFlowers();
     }
 }
