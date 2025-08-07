f628cd126c344fa7d03ef57d74513b28dbd5958a
北山_Besson <137259636+BeiShanair@users.noreply.github.com>
2025-08-07 15:42:35 +0800

    ore gen

--- src/main/java/com/besson/tutorial/world/ModConfiguredFeatures.java
+++ src/main/java/com/besson/tutorial/world/ModConfiguredFeatures.java
@@ -2,9 +2,14 @@ package com.besson.tutorial.world;
 
 import com.besson.tutorial.TutorialMod;
 import com.besson.tutorial.block.ModBlocks;
+import net.minecraft.block.Blocks;
 import net.minecraft.registry.Registerable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.structure.rule.BlockMatchRuleTest;
+import net.minecraft.structure.rule.RuleTest;
+import net.minecraft.structure.rule.TagMatchRuleTest;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.ConstantIntProvider;
 import net.minecraft.world.gen.feature.*;
@@ -13,10 +18,16 @@ import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
 import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
 
+import java.util.List;
+
 public class ModConfiguredFeatures {
     public static final RegistryKey<ConfiguredFeature<?, ?>> ICE_ETHER_TREE_KEY = of("ice_ether_tree");
 
     public static final RegistryKey<ConfiguredFeature<?, ?>> SIMPLE_FLOWER_KEY = of("simple_flower");
+
+    public static final RegistryKey<ConfiguredFeature<?, ?>> ICE_ETHER_ORE_KEY = of("ice_ether_ore");
+    public static final RegistryKey<ConfiguredFeature<?, ?>> NETHER_ICE_ETHER_ORE_KEY = of("nether_ice_ether_ore");
+    public static final RegistryKey<ConfiguredFeature<?, ?>> END_ICE_ETHER_ORE_KEY = of("end_ice_ether_ore");
 
     public static void bootstrap(Registerable<ConfiguredFeature<?, ?>> featureRegisterable) {
         ConfiguredFeatures.register(featureRegisterable, ICE_ETHER_TREE_KEY, Feature.TREE,
@@ -32,6 +43,30 @@ public class ModConfiguredFeatures {
                 new RandomPatchFeatureConfig(20, 4, 3,
                         PlacedFeatures.createEntry(Feature.SIMPLE_BLOCK,
                                 new SimpleBlockFeatureConfig(BlockStateProvider.of(ModBlocks.SIMPLE_FLOWER)))));
+
+        RuleTest stoneReplace = new TagMatchRuleTest(BlockTags.STONE_ORE_REPLACEABLES);
+        RuleTest deepslateReplace = new TagMatchRuleTest(BlockTags.DEEPSLATE_ORE_REPLACEABLES);
+        RuleTest netherReplace = new TagMatchRuleTest(BlockTags.BASE_STONE_NETHER);
+        RuleTest endReplace = new BlockMatchRuleTest(Blocks.END_STONE);
+
+        List<OreFeatureConfig.Target> overWorldTarget = List.of(
+                OreFeatureConfig.createTarget(stoneReplace, ModBlocks.ICE_ETHER_ORE.getDefaultState()),
+                OreFeatureConfig.createTarget(deepslateReplace, ModBlocks.ICE_ETHER_ORE.getDefaultState())
+        );
+        List<OreFeatureConfig.Target> netherTarget = List.of(
+                OreFeatureConfig.createTarget(netherReplace, ModBlocks.ICE_ETHER_ORE.getDefaultState())
+        );
+        List<OreFeatureConfig.Target> endTarget = List.of(
+                OreFeatureConfig.createTarget(endReplace, ModBlocks.ICE_ETHER_ORE.getDefaultState())
+        );
+
+        ConfiguredFeatures.register(featureRegisterable, ICE_ETHER_ORE_KEY, Feature.ORE,
+                new OreFeatureConfig(overWorldTarget, 9));
+        ConfiguredFeatures.register(featureRegisterable, NETHER_ICE_ETHER_ORE_KEY, Feature.ORE,
+                new OreFeatureConfig(netherTarget, 12));
+        ConfiguredFeatures.register(featureRegisterable, END_ICE_ETHER_ORE_KEY, Feature.ORE,
+                new OreFeatureConfig(endTarget, 10));
+
     }
 
     public static RegistryKey<ConfiguredFeature<?, ?>> of(String id) {
--- src/main/java/com/besson/tutorial/world/ModWorldGeneration.java
+++ src/main/java/com/besson/tutorial/world/ModWorldGeneration.java
@@ -1,11 +1,13 @@
 package com.besson.tutorial.world;
 
 import com.besson.tutorial.world.flower.ModFlowerGeneration;
+import com.besson.tutorial.world.ore.ModOreGeneration;
 import com.besson.tutorial.world.tree.ModTreeGeneration;
 
 public class ModWorldGeneration {
     public static void register(){
         ModTreeGeneration.registerTrees();
         ModFlowerGeneration.registerFlowers();
+        ModOreGeneration.registerOres();
     }
 }
